a7bf2d2288ef15218142efc0df2a2f96023d5ef6
Eric Daigo Nishihira <101433822+EricDaigo@users.noreply.github.com>
2022-09-23 00:26:36 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main {
+  public static double media(double... numeros){
+    double total = 0.0;
+
+    for(double d : numeros){
+      total += d;
+    }
+
+    return total / numeros.length;
+  }
+  
+  
+  public static void main(String[] args) {
+    double d1 = 10.0;
+    double d2 = 20.0;
+    double d3 = 30.0;
+    double d4 = 40.0;
+
+    System.out.printf("d1 = %.1f%nd2 = %.1f%nd3 = %.1f%nd4 = %.1f%n", d1, d2, d3, d4);
+
+    System.out.println();
+
+    System.out.printf("Média de d1 e d2 é: %.1f%n", media(d1, d2));
+
+    System.out.printf("Média de d1, d2 e d3 é: %.1f%n", media(d1, d2, d3));
+
+    System.out.printf("Média de d1, d2, d3 e d4 é: %.1f%n", media(d1, d2, d3, d4));
+    
+  }
+}
